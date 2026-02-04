fbb05b4fe0e287fbcebd7ec2ee349609277012ec
winzfs <jazzhjm@gmail.com>
2026-02-04 12:53:01 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js
+export const CONFIG = {
+    // 관리자 설정
+    ADMIN_EMAIL: "jazzhjm@gmail.com",
+
+    // 테스트용 계정 리스트 (개발 시 빠른 로그인을 위해 사용)
+    TEST_ACCOUNTS: [
+        { email: "test1@test.com", pw: "anjfqhk@@3" },
+        { email: "test2@test.com", pw: "anjfqhk@@3" }
+    ],
+
+    // 기타 설정
+    VERSION: "1.0.0"
+};
